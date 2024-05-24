cc0debb0fb0428c2ef392fa0e77ccfcc380ba421
Fakrul Islam <dev.rupok02@gmail.com>
2024-05-24 17:08:21 +0600

    Create Data & add user list

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        Title: "Software Engineer",
+        description: "Software engineer skilled in developing scalable, efficient, user-friendly applications.",
+        id: 1
+    },
+    {
+        Title: "Civil Engineer",
+        description: "Civil engineer experienced in designing and managing infrastructure projects.",
+        id: 1
+    },
+    {
+        Title: "Electrical Engineer",
+        description: "Electrical engineer skilled in designing and implementing innovative electrical systems.",
+        id: 1
+    }
+]
