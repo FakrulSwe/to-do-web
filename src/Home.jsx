b15b53529f07933fc751a19e3336ce72081ab262
Fakrul Islam <dev.rupok02@gmail.com>
2024-05-25 19:06:56 +0600

    Added pagination

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom';
 import { deleteUser } from './UserReducer';
 
 const Home = () => {
     const users = useSelector((state) => state.users);
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const recordsPerpage = 5;
+    const lastIndex = currentPage * recordsPerpage;
+    const firstIndex = lastIndex - recordsPerpage;
+    const records = users.slice(firstIndex, lastIndex);
+    const npage = Math.ceil(users.length/recordsPerpage);
+    const numbers = [...Array(npage + 1).keys()].slice(1);
+
     const dispatch = useDispatch();
 
     const handleDelete = (id) =>{
@@ -25,7 +34,7 @@ const Home = () => {
                 </tr>
             </thead>
             <tbody>
-                {users.map((user, index) =>(
+                {records.map((user, index) =>(
                     <tr key={index}>
                         <td>{user.id}</td>
                         <td>{user.title}</td>
@@ -38,8 +47,44 @@ const Home = () => {
                 ))}
             </tbody>
         </table>
+        
+        <div className='mt-4 paginationbottom'>
+            <nav>
+                <ul className='pagination'>
+                    <li className='page-item'>
+                        <a href="#" className='page-link'
+                        onClick={prePage}>Prev</a>
+                    </li>
+                    {
+                        numbers.map((n, i) => (
+                            <li className={`page-item ${currentPage === n ? 'active' : ''}`} key={i}>
+                                <a href="#" className='page-link'
+                                onClick={() => changeCPage(n)}>{n}</a>
+                            </li>
+                        ))
+                    }
+                    <li className='page-item'>
+                        <a href="#" className='page-link'
+                        onClick={nextPage}>Next</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
   )
+  function prePage(){
+    if(currentPage !== 1) {
+        setCurrentPage(currentPage - 1);
+    }
+  }
+  function changeCPage(id){
+    setCurrentPage(id);
+  }
+  function nextPage(id){
+    if(currentPage !== npage){
+        setCurrentPage(currentPage + 1)
+    }
+  }
 }
 
 export default Home
